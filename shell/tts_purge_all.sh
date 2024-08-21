3cf5f55ea41f153f2066f4f07b39d492c7083b32
jazzyisj <hassio.jazzyisj@gmail.com>
2024-08-20 23:35:17 -0400

    Update to 2024.8.2
    - update tts cache purge shell_command
    - update precipitation statistics
    - separate media playlist/radio

--- shell/tts_purge_all.sh
+++ shell/tts_purge_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # https://www.vionblog.com/linux-delete-files-older-than-x-days/
-find /config/tts/ -type f -name '*.wav' -exec rm {} \;
+find /config/tts/ -type f -name '*.mp3' -exec rm {} \;
--- shell/tts_purge_old.sh
+++ shell/tts_purge_old.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # https://www.vionblog.com/linux-delete-files-older-than-x-days/
-find /config/tts/ -type f -name '*.wav' -mtime +7 -exec rm {} \;
+find /config/tts/ -type f -name '*.mp3' -mtime +7 -exec rm {} \;
